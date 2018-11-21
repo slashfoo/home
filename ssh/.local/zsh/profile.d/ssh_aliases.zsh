6b95e79576322c370c7f86c33f61991fa4b29197
Jamiel Almeida <jamiel.almeida@gmail.com>
2018-11-21 15:44:57 -0800

    add ssh/

---
+++ ssh/.local/zsh/profile.d/ssh_aliases.zsh
@@ -0,0 +1,3 @@
+alias ssh-gen-config='\assemble-config -v -f ~/.ssh/config -V sshconfig'
+alias ssh-noctrl='\ssh -o ControlMaster=no -o ControlPath=none'
+alias ssh-yolo='\ssh -o ControlMaster=no -o ControlPath=none'
