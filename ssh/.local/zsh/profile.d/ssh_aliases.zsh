26d196cf59ec990c1d6f4380dfa2e1510a794159
Jamiel Almeida <jamiel.almeida@gmail.com>
2018-11-23 10:26:50 -0800

    add ssh-gen-config and i3-gen-config scripts to ssh/ and i3/ respectively

--- ssh/.local/zsh/profile.d/ssh_aliases.zsh
+++ ssh/.local/zsh/profile.d/ssh_aliases.zsh
@@ -1,3 +1,2 @@
-alias ssh-gen-config='\save-a-copy ~/.ssh/config && \assemble-config -v -F -f ~/.ssh/config -V sshconfig'
 alias ssh-noctrl='\ssh -o ControlMaster=no -o ControlPath=none'
 alias ssh-yolo='\ssh -o ControlMaster=no -o ControlPath=none'
